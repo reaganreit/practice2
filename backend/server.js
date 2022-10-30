4478d808ab25b7956086d7e44770e36141457680
alexkeener318 <alexkeener318@gmail.com>
2022-10-30 18:59:33 -0500

    Adds skeleton framework for my functions to complete

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+
+
+// adding new items to menu
+function addMenu() {
+    // have text entry points 
+    // get text from these fields
+    // connect to database
+    // send in query
+}
+
+// send orders to database
+function sendOrder() {
+    // get order items
+    // get time
+    // get name
+    // format query
+    // execute query
+}
+
+// get price and tax details
+function updatePrice() {
+    // calculate item total
+    // calculate tax
+    // calculate order total
+    // return tax and order total
+}
+
