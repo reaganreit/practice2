ca7312d5f9a7af27f1479e47b2bf4ec0339a76e4
alexkeener318 <alexkeener318@gmail.com>
2022-10-30 22:55:28 -0500

    Adds some basic code to updatePrice function

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 
 // adding new items to menu
 function addMenu() {
-    // have text entry points 
+    // have text entry points - will get these from front end code
     // get text from these fields
     // connect to database
     // send in query
@@ -18,10 +18,18 @@ function sendOrder() {
 }
 
 // get price and tax details
-function updatePrice() {
+function updatePrice(currTotal) {
     // calculate item total
+    let itemPrice = 0.00;
+        // get new order item's price from database
     // calculate tax
+    currTotal += itemPrice;
+    let taxPrice = currTotal * 0.0825;
     // calculate order total
+    let orderTotal = currTotal + taxPrice;
     // return tax and order total
+    // may have to make these variables global variables
+    let result = {orderTotal, taxPrice};
+    return result;
 }
 
