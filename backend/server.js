c6da5d2bc250d02139507b16788f1b531a811f53
alexkeener318 <alexkeener318@gmail.com>
2022-11-04 15:42:26 -0500

    Gives the updatePrice function functionality

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // connect to the database
+const { query } = require('express');
 const express = require('../node_modules/express');
 const { Pool } = require('../node_modules/pg');
 const dotenv = require('../node_modules/dotenv').config();
@@ -14,15 +15,24 @@ const pool = new Pool({
     port: process.env.PSQL_PORT,
     ssl: {rejectUnauthorized: false}
 });
+let price;
+let test = new Date().toString("dd-MM-yyyy HH:mm");
+let date = new Date().toLocaleDateString();
+let time = new Date().toLocaleTimeString();
+time.substring(0, time.length-4);
+let updatedDate = date + " " + time;
+console.log("Date: " + test);
 teammembers = [];
     pool
-        .query('SELECT * FROM teammembers;')
+    .query("SELECT item_price FROM menu WHERE item_name ='Garlic Fries';")
         .then(query_res => {
             for (let i = 0; i < query_res.rowCount; i++){
+                console.log(i);
                 teammembers.push(query_res.rows[i]);
                 console.log(query_res.rows[i]);
+                price = query_res.rows[i][0];
             }});
-
+           
 
 const Order = (()=>{
     let orderItems = "";
@@ -38,10 +48,19 @@ const Order = (()=>{
     }
 
     // get price and tax details
-    const updatePrice = () => {
+    const updatePrice = (itemName) => {
         // calculate item total
         let itemPrice = 0.00;
             // get new order item's price from database
+            let price;
+            pool
+            .query("SELECT item_price FROM menu WHERE item_name ='" + itemName + "';")
+            .then(query_res => {
+                for (let i = 0; i < query_res.rowCount; i++){
+                    price = query_res.rows[i];
+                    console.log(query_res.rows[i]);
+                }});
+            itemPrice = price.item_price;
         // calculate tax
         currTotal += itemPrice;
         let taxPrice = currTotal * 0.0825;
@@ -53,26 +72,33 @@ const Order = (()=>{
     }
 });
 
-
 // adding new items to menu - will probably change this to an event listener tied to a button
-function addMenu() {
-    // have text entry points - will get these from front end code
-    // get text from these fields
-    let itemName = // get name from entry
-    let itemPrice = // get from entry
-    let itemIngreds = // get from entry
-    // connect to database
-    // send in query
-    const query = "INSERT INTO menu VALUES('" + itemName +"', " + itemPrice +", '" + itemIngreds + "');";
-    let result = pgClient.query(query);
-}
+// function addMenu() {
+//     // have text entry points - will get these from front end code
+//     // get text from these fields
+//     let itemName = // get name from entry
+//     let itemPrice = // get from entry
+//     let itemIngreds = // get from entry
+//     // connect to database
+//     // send in query
+//     const query = "INSERT INTO menu VALUES('" + itemName +"', " + itemPrice +", '" + itemIngreds + "');";
+//     let result = pgClient.query(query);
+// }
 
 // send orders to database
 function sendOrder(orderItems) {
     // get order items
     // get time
+    let date = new Date().toLocaleDateString();
     // get name
     // format query
+    // for receipt: order_id, payment_type, total, date/timestamp, order_items, customer_name, card_number, employeee_name
+    // for orders: order_id, total, timestamp
+    let query = "INSERT INTO orders values";
     // execute query
+    pool.query(query);
+
+    // call updateInventory
+    updateInventory(orderItems);
 }
 
