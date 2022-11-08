4a1c8c1059faa30b658bace176706c7699e902df
Emily Ha <emilyvyha@gmail.com>
2022-11-08 16:09:26 -0600

    finished implementing shopping cart for customers and cashiers and creating HTTP requests for sending in orders

--- frontend/src/Pages/CustomerGUI.js
+++ frontend/src/Pages/CustomerGUI.js
@@ -96,12 +96,39 @@ const CustomerGUI = () => {
         }
     };
 
+    const handleCheckout = async (payment, employeeName) => {
+        setIsLoading(true);
+        emptyReceipt()
+        try {
+            const response = await fetch('http://localhost:5000/sendOrder', {
+                method: 'POST',
+                body: JSON.stringify({ paymentType : payment, empName: employeeName }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error! status: ${response.status}`);
+            }
+        
+            const result = await response.json();
+            console.log(result);
+
+        } catch (err) {
+            setErr(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const emptyReceipt = () => {
+        setReceipt([]);
+    };
+
     return (
         <div style = {{ width: "90%", height: "100%", marginLeft: "5%" }}>
-
-            <globalTotal.Provider value = {{ total, setTotal }}>
-                <Checkout />
-            </globalTotal.Provider>
 
             <div className="menuOptions" style={{ height: "7.5%", marginTop: "2.5%" }}>
                 <Button onClick={bowlMenu} style = {{ height: "100%", width: "17.5%", marginRight: "7%", marginLeft: "4.5%", backgroundColor: "blue", color: "white" }}>Bowl</Button>
@@ -141,10 +168,12 @@ const CustomerGUI = () => {
                                 Total: $ { total }
                             </p>
                         </div>
-                        <Link to="/checkout" style={{ textDecoration:"none" }}>
-                            <Button onClick = {() => setTotal(total)} style = {{ height: "25%", width: "80%", marginTop: "10%", marginLeft: "10%", backgroundColor: "blue", color: "white" }}>Checkout</Button>
-                        </Link>
+                        <div className="checkoutButtons" style = {{ width:"80%", marginLeft: "10%" }}>
+                            <Button onClick = {event => handleCheckout("Credit", "customer")} style = {{ height: "47.5%", width: "47.5%", marginTop: "2.5%", marginLeft: "1.66%", backgroundColor: "blue", color: "white" }}>Credit</Button>
+                            <Button onClick = {event => handleCheckout("Dining Dollars", "customer")} style = {{ height: "47.5%", width: "47.5%", marginTop: "2.5%", marginLeft: "1.66%", backgroundColor: "blue", color: "white" }}>Dining Dollars</Button>
+                        </div>
                     </div>
+                    
                     <Link to="/pinpad" style={{ textDecoration:"none" }}>
                         <Button style = {{ maxHeight: "25%", width: "60%", marginTop: "2.5%", marginLeft: "20%", backgroundColor: "red", color: "white" }}>Sign In</Button>
                     </Link>
